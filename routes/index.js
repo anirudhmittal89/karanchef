f3ce2183cdb15f2abe7bebbf022aacfb9530bf9a
Mittal <anirudh.mittal89@gmail.com>
2013-09-17 23:49:01 -0700

    initial

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,16 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Karan the Chef' })
 };
+
+exports.aboutme = function(req, res){
+	var ob=[{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" },
+	{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" },
+	{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" },
+	{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" },
+	{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" },
+	{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" },
+	{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" },
+	{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" } ]
+
+  res.render('aboutme', {ob:ob})
+};
