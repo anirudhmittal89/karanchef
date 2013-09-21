cff0ff83ff0cd839329c8a7260f44025ad2fa63a
Mittal <anirudh.mittal89@gmail.com>
2013-09-21 00:55:20 -0700

    added isotope properly

---
+++ public/javascripts/self.js
@@ -0,0 +1,57 @@
+$(window).load(function() {
+/*		$("#columns").masonry({
+
+			itemSelector:".pin",
+			columnWidth: 135,
+			   isFitWidth: true
+		});imagesLoaded(function() {
+   $('#columns').masonry('reload');
+  });
+*/
+/*
+var $container = $('#columns');
+
+$container.isotope({
+    itemSelector : '#columns div.pin',
+});
+
+
+$('#ncont a').click(function(){
+    $.scrollTo( '#fcont', 1200, {axis:'y'} );   
+});
+
+$('#filters a').click(function(){
+    var selector = $(this).attr('data-filter');
+    $container.isotope({ filter: selector });
+    return false;
+});
+*/
+
+    var $container = $('.portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
+
+
+	});
--- routes/index.js
+++ routes/index.js
@@ -17,22 +17,20 @@ exports.me = function(req, res){
 
 exports.recipes = function(req, res){
 	var ob=[
-	{ "src":"/images/food/13.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/2.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/3.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/4.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/5.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/6.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/7.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/8.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/9.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/10.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/11.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/12.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/13.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"/images/food/14.jpg" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" },
-	{ "src":"http://cssdeck.com/uploads/media/items/2/2v3VhAp.png" , "caption":"Lorem ipsum dolor sit amet, con" } ]
+	{ "src":"/images/food/13.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"snacks"},
+	{ "src":"/images/food/2.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"desert"},
+	{ "src":"/images/food/3.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"main"},
+	{ "src":"/images/food/4.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"main"},
+	{ "src":"/images/food/5.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"main"},
+	{ "src":"/images/food/6.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"main"},
+	{ "src":"/images/food/7.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"desert"},
+	{ "src":"/images/food/8.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"main"},
+	{ "src":"/images/food/9.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"desert"},
+	{ "src":"/images/food/10.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"main"},
+	{ "src":"/images/food/11.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"desert"},
+	{ "src":"/images/food/12.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"desert"},
+	{ "src":"/images/food/13.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"desert"},
+	{ "src":"/images/food/14.jpg" , "caption":"Lorem ipsum dolor sit amet, con" ,"categ":"desert"}]
 
   res.render('recipes', {ob:ob})
 };
