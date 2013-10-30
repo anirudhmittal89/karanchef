fb068ff8e4c93dc78b974f7a58594aeb0f003569
Mittal <anirudh.mittal89@gmail.com>
2013-10-29 19:38:26 -0700

    dynamic links for haeder

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ express.compiler.compilers.less.compile = function(str, fn){
 
 app.get('/', routes.index);
 app.get('/index', routes.index);
-app.get('/recipes', routes.recipes);
+app.get('/*/recipes', routes.recipes);
 app.get('/*/info', routes.info);
 app.get('/me', routes.me);
 var port = process.env.PORT || 5000;
